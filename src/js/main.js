b55a015dd0768a16c7c9f0b9ec70092552ac13f7
YWT-XPS14Z\ywtstewart <yuraymar.stewart@gmail.com>
2018-02-09 16:15:24 +0100

    Beautify main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 import Player from "./player";
 import Card from "./card";
 
-  const cards = [],
+const cards = [],
     players = [],
     deelStapel = [],
     aflegStapel = [],
@@ -25,168 +25,169 @@ import Card from "./card";
     ],
     people = ["Alice", "Bob", "Carol", "Jay"];
 
-    function write(data){
-document.write(data);
-    }
 
-  function log(message) {
-    write('<p>' + message + '</p>');
+function write(data) {
+  document.write(data);
+}
 
-  }
+function log(message) {
+  write('<p>' + message + '</p>');
 
-  function logPlayed(name, card){
-   write('<p>' + name + ' palyed ' + card +'</p>');
-  }
+}
 
-  function matchSuitColor(cards1) {
-    for (let card in cards1) {
-      if (cards1[card].suitColor == aflegStapel[aflegStapel.length - 1].suitColor) {
-        return [cards1[card], card];
-      } else {
-        return false;
-      }
-    }
-  }
+function logPlayed(name, card) {
+  write('<p>' + name + ' palyed ' + card + '</p>');
+}
 
-  function matchCardNumber(cards1) {
-    for (let card in cards1) {
-      if (cards1[card].cardNumber == aflegStapel[aflegStapel.length - 1].cardNumber) {
-        return [cards1[card], card];
-      } else {
-        return false;
-      }
+function matchSuitColor(cards1) {
+  for (let card in cards1) {
+    if (cards1[card].suitColor == aflegStapel[aflegStapel.length - 1].suitColor) {
+      return [cards1[card], card];
+    } else {
+      return false;
     }
   }
+}
 
-  function createCards() {
-    for (let suit in suits) {
-      for (let value in values) {
-        let card = new Card(values[value], suits[suit])
-        cards.push(card);
-      }
+function matchCardNumber(cards1) {
+  for (let card in cards1) {
+    if (cards1[card].cardNumber == aflegStapel[aflegStapel.length - 1].cardNumber) {
+      return [cards1[card], card];
+    } else {
+      return false;
     }
   }
+}
 
-  function createPlayers() {
-    for (let person in people) {
-      let player = new Player(people[person])
-      players.push(player)
+function createCards() {
+  for (let suit in suits) {
+    for (let value in values) {
+      let card = new Card(values[value], suits[suit])
+      cards.push(card);
     }
   }
+}
 
-  function shuffleCards(cards) {
-    cards.sort(function() {
-      return 0.5 - Math.random()
-    })
-    return cards;
+function createPlayers() {
+  for (let person in people) {
+    let player = new Player(people[person])
+    players.push(player)
+  }
+}
+
+function shuffleCards(cards) {
+  cards.sort(function() {
+    return 0.5 - Math.random()
+  })
+  return cards;
+}
+
+function dealCards() {
+  for (let player in players) {
+    for (let i = 0; i < 7; i++) {
+      let rndm = Math.floor((Math.random() * 7) + 1);
+      players[player].cards.push(cards[rndm]);
+      cards.splice(rndm, 1);
+    }
+    log(players[player].getName + ' has been dealt: ' + getPlayerCards(players[player].getCards));
   }
 
-  function dealCards() {
+  deelStapel.push(...cards);
+
+}
+
+function getPlayerCards(cards) {
+  let cardsString = ' ';
+  for (let card in cards) {
+    cardsString += cards[card].toString;
+    cardsString += ' - ';
+
+  }
+  return cardsString;
+}
+
+function setTopCard() {
+  aflegStapel.push(deelStapel[0]);
+  deelStapel.splice(0, 1);
+  log('Top Card is: ' + aflegStapel[0].toString);
+
+}
+
+function getPlayersNames() {
+  let playersString = '';
+  if (players) {
     for (let player in players) {
-      for (let i = 0; i < 7; i++) {
-        let rndm = Math.floor((Math.random() * 7) + 1);
-        players[player].cards.push(cards[rndm]);
-        cards.splice(rndm, 1);
-      }
-      log(players[player].getName + ' has been dealt: ' + getPlayerCards(players[player].getCards));
-    }
-
-    deelStapel.push(...cards);
-
-  }
-
-  function getPlayerCards(cards) {
-    let cardsString = ' ';
-    for (let card in cards) {
-      cardsString += cards[card].toString;
-      cardsString += ' - ';
-
-    }
-    return cardsString;
-  }
-
-  function setTopCard() {
-    aflegStapel.push(deelStapel[0]);
-    deelStapel.splice(0, 1);
-    log('Top Card is: ' + aflegStapel[0].toString);
-
-  }
-
-  function getPlayersNames() {
-    let playersString = '';
-    if (players) {
-      for (let player in players) {
-        playersString += players[player].getName;
-        if (player < players.length - 1) {
-          playersString += ', '
-        }
+      playersString += players[player].getName;
+      if (player < players.length - 1) {
+        playersString += ', '
       }
     }
-    return playersString;
   }
+  return playersString;
+}
 
-  function initGame() {
-    createCards();
-    createPlayers();
-    shuffleCards(cards);
-    let playerNames = getPlayersNames();
-    log('The game has been started with ' + playerNames)
-    dealCards();
-    setTopCard();
-    playGame();
-  }
+function initGame() {
+  createCards();
+  createPlayers();
+  shuffleCards(cards);
+  let playerNames = getPlayersNames();
+  log('The game has been started with ' + playerNames)
+  dealCards();
+  setTopCard();
+  playGame();
+}
 
-  function playGame() {
-    let i = 0;
+function playGame() {
+  let i = 0;
 
-    do {
-      i++
+  do {
+    i++
 
-      if (i == players.length) {
-        i = 0;
-      }
-
-      let suitColorMatch = matchSuitColor(players[i].getCards, aflegStapel[aflegStapel.length - 1]);
-      let cardNumberMatch = matchCardNumber(players[i].getCards, aflegStapel[aflegStapel.length - 1]);
-
-      if (typeof cardNumberMatch != 'boolean') {
-        aflegStapel.push(cardNumberMatch[0]);
-        players[i].cards.splice(cardNumberMatch[1], 1);
-        logPlayed(players[i].getName,cardNumberMatch[0].toString);
-
-      } else if (typeof suitColorMatch != 'boolean') {
-        aflegStapel.push(suitColorMatch[0]);
-        players[i].cards.splice(suitColorMatch[1], 1);
-        logPlayed(players[i].getName,suitColorMatch[0].toString);
-
-      } else if (typeof suitColorMatch == 'boolean' && typeof cardNumberMatch == 'boolean') {
-        let nextCard =  deelStapel[0].toString || ''
-        log(players[i].getName + ' does not have a suitable card, taking from deck ' + nextCard );
-        players[i].cards.push(deelStapel[0]);
-        deelStapel.splice(0, 1);
-
-        deelStapel.push(...aflegStapel);
-        shuffleCards(deelStapel);
-        aflegStapel.splice(0, aflegStapel.length);
-        setTopCard();
-
-      } else {
-        log('Oops someting went wrong!')
-      }
-
-      if (players[i].getCards.length == 1) {
-        log(players[i].getName + " has 1 card remaining!")
-      }
-
-      if (players[i].getCards.length == 0) {
-        winner.push(players[i]);
-      }
-
-    } while (winner.length == 0)
-
-    if (winner.length > 0) {
-      log(winner[0].getName + ' has won.');
+    if (i == players.length) {
+      i = 0;
     }
-  }
 
-   initGame()
+    let suitColorMatch = matchSuitColor(players[i].getCards, aflegStapel[aflegStapel.length - 1]);
+    let cardNumberMatch = matchCardNumber(players[i].getCards, aflegStapel[aflegStapel.length - 1]);
+
+    if (typeof cardNumberMatch != 'boolean') {
+      aflegStapel.push(cardNumberMatch[0]);
+      players[i].cards.splice(cardNumberMatch[1], 1);
+      logPlayed(players[i].getName, cardNumberMatch[0].toString);
+
+    } else if (typeof suitColorMatch != 'boolean') {
+      aflegStapel.push(suitColorMatch[0]);
+      players[i].cards.splice(suitColorMatch[1], 1);
+      logPlayed(players[i].getName, suitColorMatch[0].toString);
+
+    } else if (typeof suitColorMatch == 'boolean' && typeof cardNumberMatch == 'boolean') {
+      let nextCard = deelStapel[0].toString || ''
+      log(players[i].getName + ' does not have a suitable card, taking from deck ' + nextCard);
+      players[i].cards.push(deelStapel[0]);
+      deelStapel.splice(0, 1);
+
+      deelStapel.push(...aflegStapel);
+      shuffleCards(deelStapel);
+      aflegStapel.splice(0, aflegStapel.length);
+      setTopCard();
+
+    } else {
+      log('Oops someting went wrong!')
+    }
+
+    if (players[i].getCards.length == 1) {
+      log(players[i].getName + " has 1 card remaining!")
+    }
+
+    if (players[i].getCards.length == 0) {
+      winner.push(players[i]);
+    }
+
+  } while (winner.length == 0)
+
+  if (winner.length > 0) {
+    log(winner[0].getName + ' has won.');
+  }
+}
+
+initGame()
